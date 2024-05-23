7283ebffa3415eaf9ed43886df854f38c2f49df3
Finnedorn <daniele.giuntoli@outlook.com>
2024-05-23 15:54:25 +0200

    added fetch call to API / added function to show on page json content through <ul>

--- exercise.js
+++ exercise.js
@@ -7,6 +7,7 @@ const path = require("path");
 function accessJSON(file){
     const filePath = path.join(__dirname, file + ".json");
     const fileData = fs.readFileSync(filePath, "utf-8");
+    if(fileData === "") return [];
     return JSON.parse(fileData);
 }
 
@@ -16,5 +17,33 @@ function writeOnJSON(file, newData) {
     fs.writeFileSync(filePath, fileToString);
 }
 
+const server = http.createServer( (req, res) => {
+    const norrisData = accessJSON("norrisDB");
+    if (req.url === "/favicon.ico") {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end();
+        return;
+    }
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    fetch("https://api.chucknorris.io/jokes/random")
+        .then(response => response.json())
+        .then(data => {
+            let page = "<ul>";
+            const randomJoke = data.value;
+            if(!norrisData.includes(randomJoke)){
+                norrisData.push(randomJoke);
+                writeOnJSON("norrisDB", norrisData);
+            }
+            norrisData.forEach(element => {
+                page += `<li>${element}</li>`;
+            });
+            page += "</ul>";
+            res.end(page);
+        })
 
+});
+
+server.listen(port, host, ()=>{
+    console.log(`Ho avviato il server su http://${host}:${port}`)
+});
 
